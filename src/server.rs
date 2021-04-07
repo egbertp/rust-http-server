2373d016e9a67de19c9328c35f3741d18af9dc3f
Egbert Pot <egbert@egbertpot.nl>
2021-04-07 22:27:42 +0200

    Server capable of listening to port

--- src/server.rs
+++ src/server.rs
@@ -1,11 +1,9 @@
+use std::io::Read;
 use std::net::TcpListener;
 
 pub struct Server {
   addr: String,
 }
-
-// A reference to an array of arbitrary size, i.e. &[u8] is a pointer
-fn arr(a: &[u8]) {}
 
 impl Server {
   pub fn new(addr: String) -> Self {
@@ -17,12 +15,19 @@ impl Server {
 
     let listener = TcpListener::bind(&self.addr).unwrap();
 
-    match listener.accept() {
-      Ok((mut stream, _)) => {
-        let a = [1, 3, 4, 5];
-        arr(&a); //This is called a slice
+    loop {
+      match listener.accept() {
+        Ok((mut stream, _)) => {
+          let mut buffer = [0; 1024]; //Create an array with 1024 bytes
+          match stream.read(&mut buffer) {
+            Ok(_) => {
+              println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+            }
+            Err(e) => println!("Failed to read from connection: {}", e),
+          }
+        }
+        Err(e) => println!("Failed to establish a connection: {}", e),
       }
-      Err(e) => println!("Failed to establish a connection: {}", e),
     }
   }
 }
