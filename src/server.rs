e273b13bb1620177da7430f603c7909e82624293
Egbert Pot <egbert@egbertpot.nl>
2021-04-07 21:54:33 +0200

    Implemented TCP listener

--- src/server.rs
+++ src/server.rs
@@ -1,13 +1,26 @@
+use std::net::TcpListener;
+
 pub struct Server {
   addr: String,
 }
 
 impl Server {
   pub fn new(addr: String) -> Self {
-    Self { addr: addr }
+    Self { addr }
   }
 
   pub fn run(self) {
-    println!("Listening on {}", self.addr)
+    println!("Listening on {}", self.addr);
+
+    let listener = TcpListener::bind(&self.addr).unwrap();
+
+    loop {
+      let res = listener.accept();
+      if res.is_err() {
+        continue;
+      }
+
+      let (stream, addr) = res.unwrap();
+    }
   }
 }
